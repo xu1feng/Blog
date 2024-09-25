9b19b74e3e946ee66b6b4c124445825174454333
xu1feng <2465549609@qq.com>
2024-09-25 23:02:31 +0800

    博客前台模块-发送文章评论功能代码实现

--- sangeng-blog/src/main/java/com/sangeng/controller/CommentController.java
+++ sangeng-blog/src/main/java/com/sangeng/controller/CommentController.java
@@ -1,11 +1,10 @@
 package com.sangeng.controller;
 
 import com.sangeng.domain.ResponseResult;
+import com.sangeng.domain.entity.Comment;
 import com.sangeng.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @Description:
@@ -23,5 +22,10 @@ public class CommentController {
     @GetMapping("/commentList")
     public ResponseResult commentList(Long articleId, Integer pageNum, Integer pageSize) {
         return commentService.commentList(articleId, pageNum, pageSize);
+    }
+
+    @PostMapping
+    public ResponseResult addComment(@RequestBody Comment comment) {
+        return commentService.addComment(comment);
     }
 }
